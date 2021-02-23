3e0e9ef546ae305525974ae0e7bdd6941116904a
joevtap <joelvitortorres@gmail.com>
2021-02-23 17:19:13 -0300

    Update

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
 import styles from '../styles/components/ExperienceBar.module.scss'
 
 export function ExperienceBar() {
+
+    const [value, setValue] = useState(50)
+
     return (
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: '50%' }} />
-                <span className={styles.currentExperience} style={{ left: "50%" }}>300 xp</span>
+                <div style={{ width: `${value}%` }} />
+                <span className={styles.currentExperience} style={{ left: `${value}%` }}>{Number(value) * 6}xp</span>
             </div>
             <span>600 xp</span>
         </header>
