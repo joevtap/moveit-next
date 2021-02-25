ea3cde8b0750af5456e7fd1eb5678ac76dfdd2e4
joevtap <joelvitortorres@gmail.com>
2021-02-25 18:36:26 -0300

    Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/global.scss";
 
 import { ChallengesProvider } from "../contexts/ChallengesContext";
-import { CountdownProvider } from "../contexts/CountdownContext";
 
 function MyApp({ Component, pageProps }) {
     return (
